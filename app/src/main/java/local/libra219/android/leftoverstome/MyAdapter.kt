c3a88dfc311631d1067f878c84e31ef9e757bd4c
libra219 <15705883+libra219@users.noreply.github.com>
2020-01-31 14:02:30 +0900

    🎨 : Update class
    汎用性高めるために変更

--- app/src/main/java/local/libra219/android/leftoverstome/MyAdapter.kt
+++ app/src/main/java/local/libra219/android/leftoverstome/MyAdapter.kt
@@ -6,26 +6,26 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MyAdapter(private val myDataset: Array<String>) :
+class MyAdapter(private val myDataset: Array<String>, private val setAdapterItem: Int, private val setListView: Int) :
     RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
-        val text_View: TextView = view.findViewById(R.id.tv_card_list_title)
+    class MyViewHolder(private val view: View, setListView: Int) : RecyclerView.ViewHolder(view){
+        val text_View: TextView = view.findViewById(setListView)
     }
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
         // create a new view
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.list_view_adapter_item, parent, false)
+        val view = inflater.inflate(setAdapterItem, parent, false)
 //
 //        val textView = LayoutInflater.from(parent.context)
 //            .inflate(R.layout.list_view_adapter_item, parent, false) as TextView
         // set the view's size, margins, paddings and layout parameters
 
-        return MyViewHolder(view)
+        return MyViewHolder(view, setListView)
     }
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
