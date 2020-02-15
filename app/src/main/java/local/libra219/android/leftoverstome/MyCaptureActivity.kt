b16b13b6fbd3e8afc4a6c2eebf14993f1b04a05a
libra219 <15705883+libra219@users.noreply.github.com>
2020-02-16 05:59:47 +0900

    ✨ : Add class
    QRコード読み取り用のクラス

---
+++ app/src/main/java/local/libra219/android/leftoverstome/MyCaptureActivity.kt
@@ -0,0 +1,12 @@
+package local.libra219.android.leftoverstome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyCaptureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_capture)
+    }
+}
