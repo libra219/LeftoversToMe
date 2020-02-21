0e15adb38af2069d21758f594f3371f78d5766d5
libra219 <15705883+libra219@users.noreply.github.com>
2020-02-21 10:22:55 +0900

    ✨ : Add function
    Firebaseから画像取得して表示する

--- app/src/main/java/local/libra219/android/leftoverstome/ProductActivity.kt
+++ app/src/main/java/local/libra219/android/leftoverstome/ProductActivity.kt
@@ -1,13 +1,20 @@
 package local.libra219.android.leftoverstome
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.SimpleAdapter
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_keep_list.*
 import kotlinx.android.synthetic.main.activity_product.*
 
 class ProductActivity : AppCompatActivity() {
@@ -15,8 +22,14 @@ class ProductActivity : AppCompatActivity() {
      * 変数初期化
      */
     private var firebaseDatabase: FirebaseFirestore? = null
+    private var fs: FirebaseFirestore? = null
     private val TAG = "ProductActivity"
     private lateinit var dataStore: SharedPreferences
+    private var storage: FirebaseStorage? = null
+
+    init {
+        storage = FirebaseStorage.getInstance()
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +44,7 @@ class ProductActivity : AppCompatActivity() {
         val explanation = intent.getStringExtra("PRO_EX")
         val price = intent.getStringExtra("PRO_PRICE")
         val key = intent.getStringExtra("PRO_KEY")
+        val img = intent.getStringExtra("PRO_IMG")
 
         // SharedPreferencesインスタンスを生成
         dataStore = getSharedPreferences("DataStore", Context.MODE_PRIVATE)
@@ -41,12 +55,22 @@ class ProductActivity : AppCompatActivity() {
         tv_product_price.text = price
 
         /**
+         * 画像の挿入、Glide使用
+         * loadにURL入れると表示可能
+         */
+        val imageView = findViewById<ImageView>(R.id.img_product)
+        Glide.with(this)
+            .load(img)
+            .into(imageView)
+
+
+        /**
          * firebaseのデータベース初期化
          */
         firebaseDatabase = FirebaseFirestore.getInstance()
 
+        /** ボタン押されたとき **/
         btn_product_keep.setOnClickListener {
-            
             firebaseDatabase?.collection("item")?.document(key)
                 ?.update("keep_id", userId)
                 ?.addOnSuccessListener {
